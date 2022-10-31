bd73c76449a2fb66057663776788301b81e33fce
Jordan <jwald3@brockport.edu>
2022-10-31 16:20:37 -0400

    Created SinglePost component

---
+++ components/SinglePost.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import { urlFor } from "../sanity";
+import { Post } from "../typings";
+
+interface Props {
+    post: Post;
+}
+
+function SinglePost({ post }: Props) {
+    return (
+        <Link key={post._id} href={`/post/${post.slug.current}`}>
+            <div className="border rounded-lg group cursor-pointer overflow-hidden">
+                <img
+                    className="w-full h-60 object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out"
+                    src={urlFor(post.mainImage).url()!}
+                    alt=""
+                />
+                <div className="flex justify-between p-5 bg-white">
+                    <div className="">
+                        <p className="text-lg font-bold">{post.title}</p>
+                        <p>
+                            {post.description} by {post.author.name}
+                        </p>
+                    </div>
+                    <img
+                        className="h-12 w-12 rounded-full"
+                        src={urlFor(post.author.image).url()!}
+                        alt=""
+                    />
+                </div>
+            </div>
+        </Link>
+    );
+}
+
+export default SinglePost;
