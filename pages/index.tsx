98cd3b4eab249500ac9d92e6ef087bbab241e70b
Jordan <jwald3@brockport.edu>
2022-10-31 16:20:13 -0400

    Extracted SinglePost component from index

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import Header from "../components/Header";
+import SinglePost from "../components/SinglePost";
 import { sanityClient, urlFor } from "../sanity";
 import { Post } from "../typings";
 
@@ -40,30 +41,7 @@ export default function Home({ posts }: Props) {
             {/* Posts */}
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 lg:p-6">
                 {posts.map((post) => (
-                    <Link key={post._id} href={`/post/${post.slug.current}`}>
-                        <div className="border rounded-lg group cursor-pointer overflow-hidden">
-                            <img
-                                className="w-full h-60 object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out"
-                                src={urlFor(post.mainImage).url()!}
-                                alt=""
-                            />
-                            <div className="flex justify-between p-5 bg-white">
-                                <div className="">
-                                    <p className="text-lg font-bold">
-                                        {post.title}
-                                    </p>
-                                    <p>
-                                        {post.description} by {post.author.name}
-                                    </p>
-                                </div>
-                                <img
-                                    className="h-12 w-12 rounded-full"
-                                    src={urlFor(post.author.image).url()!}
-                                    alt=""
-                                />
-                            </div>
-                        </div>
-                    </Link>
+                    <SinglePost post={post} key={post._id} />
                 ))}
             </div>
         </div>
