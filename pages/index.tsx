ff1d625bfa2be6528b4949885aacaab06f2ee3bf
Jordan <jwald3@brockport.edu>
2022-11-01 02:38:21 -0400

    Extracted HomepageBanner component from idx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import Header from "../components/Header";
+import HomepageBanner from "../components/HomepageBanner";
 import PostContainer from "../components/PostContainer";
 import SinglePost from "../components/SinglePost";
 import { sanityClient, urlFor } from "../sanity";
@@ -22,23 +23,7 @@ export default function Home({ posts }: Props) {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <Header />
-            <div className="flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0">
-                <div className="px-10 space-y-5">
-                    <h1 className="text-6xl max-w-xl font-serif">
-                        <span className="underline decoration-black decoration-4">
-                            Medium
-                        </span>{" "}
-                        is a place to write, read, and connect
-                    </h1>
-                    <h2>
-                        It's easy and free to post your thinking on any topic
-                        and connect with millions of readers.
-                    </h2>
-                </div>
-                <div className="hidden md:inline-flex h-32 lg:h-full">
-                    <img src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png" />
-                </div>
-            </div>
+            <HomepageBanner />
             <PostContainer posts={posts} />
         </div>
     );
