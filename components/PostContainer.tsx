c6c412ee33bf578da053f5f0e8d9ac140a67c54a
Jordan <jwald3@brockport.edu>
2022-11-01 02:36:11 -0400

    Removed unneeded imports

--- components/PostContainer.tsx
+++ components/PostContainer.tsx
@@ -1,5 +1,3 @@
-import Link from "next/link";
-import { urlFor } from "../sanity";
 import { Post } from "../typings";
 import SinglePost from "./SinglePost";
 
@@ -11,7 +9,7 @@ function PostContainer({ posts }: Props) {
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 lg:p-6">
             {posts.map((pst: Post) => (
-                <SinglePost post={pst as Post} key={pst._id} />
+                <SinglePost post={pst} key={pst._id} />
             ))}
         </div>
     );
