74c7cc52c8fe9d7e1feecec1f180bf60fa55f484
Jordan <jwald3@brockport.edu>
2022-11-02 03:51:44 -0400

    Restyled home page banner

--- components/HomepageBanner.tsx
+++ components/HomepageBanner.tsx
@@ -1,20 +1,20 @@
 function HomepageBanner() {
     return (
-        <div className="flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0">
+        <div className="flex justify-between items-center h-60 bg-green-500 border-y border-black py-10 lg:py-0">
             <div className="px-10 space-y-5">
                 <h1 className="text-6xl max-w-xl font-serif">
-                    <span className="underline decoration-black decoration-4">
-                        Medium
-                    </span>{" "}
-                    is a place to write, read, and connect
+                    My journey as a software developer
                 </h1>
                 <h2>
-                    It's easy and free to post your thinking on any topic and
-                    connect with millions of readers.
+                    My stories about using programming languages to solve
+                    problems and delight users.
                 </h2>
             </div>
             <div className="hidden md:inline-flex h-32 lg:h-full">
-                <img src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png" />
+                <img
+                    src="https://user-images.githubusercontent.com/59290280/199429403-21b54b7c-a402-46b0-822d-37ffac554ee2.png"
+                    className="h-1/3 mx-10 my-auto"
+                />
             </div>
         </div>
     );
