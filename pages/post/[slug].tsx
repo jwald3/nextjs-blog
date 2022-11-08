2074714670c9273d9baa6f7882862468d8b26240
Jordan <jwald3@brockport.edu>
2022-11-08 02:04:49 -0500

    Added filler message when no comments

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -222,16 +222,25 @@ function PostPage({ post }: Props) {
                 <div className="flex flex-col p-10 my-10 max-w-2xl mx-auto shadow-green-500 shadow space-y-2">
                     <h3 className="text-4xl">Comments</h3>
                     <hr className="pb-2" />
-                    {post.comments.map((comment) => (
-                        <div key={comment._id} className="">
-                            <p className="">
-                                <span className="text-green-500">
-                                    {comment.name}
-                                </span>
-                                : {comment.comment}
+                    {post.comments.length > 0 ? (
+                        post.comments.map((comment) => (
+                            <div key={comment._id} className="">
+                                <p className="">
+                                    <span className="text-green-500">
+                                        {comment.name}
+                                    </span>
+                                    : {comment.comment}
+                                </p>
+                            </div>
+                        ))
+                    ) : (
+                        <div>
+                            <p>
+                                There are no comments yet. Be the first to leave
+                                one!
                             </p>
                         </div>
-                    ))}
+                    )}
                 </div>
             </main>
         </>
